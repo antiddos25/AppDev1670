0f3b685d0a1ab87e5a574d937b488c3c53a8cbb6
OS <OS@DESKTOP-4DIIGK3>
2022-05-10 14:23:55 +0700

    Add project files.

---
+++ FBookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FBookStore.Models;
+using Microsoft.EntityFrameworkCore;
+namespace FBookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ FBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
